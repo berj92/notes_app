8006a0e3c18959508d39402247eb600cbd6bbe85
fatima92-8 <berjawi.fatima.92@gmail.com>
2025-06-03 14:40:18 +0300

    display notes part one

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 
 import 'custom_note_item.dart';
 
@@ -7,16 +10,29 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-          padding: EdgeInsets.zero,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(vertical: 4),
-              child: NoteItem(),
-            );
-          }),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+// ana ktbt hwde mn chatgpt
+        // List<NoteModel> notes =
+        //     BlocProvider.of<NotesCubit>(context).notes ?? [];
+        // if (notes.isEmpty) {
+        //   return const Center(child: Text('No notes available.'));
+        // }
+        // hwwe keteb hyde ana ktbt yalle fw2u l2nnu 3ata error
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+              itemCount: notes.length,
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 4),
+                  child: NoteItem(),
+                );
+              }),
+        );
+      },
     );
   }
 }
